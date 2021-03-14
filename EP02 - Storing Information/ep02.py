3ac14b1465d3a0aa80d7e76a8b829502434f6949
Saswat Samal <39031660+saswatsamal@users.noreply.github.com>
2021-03-15 00:07:48 +0530

    Create ep02.py

---
+++ EP02 - Storing Information/ep02.py
@@ -0,0 +1,26 @@
+# Episode 2 of Python101 by Saswat Codes
+# https://github.com/saswatcodes/Python101 
+
+#Integer : Integer Values as we know in maths
+myInt = 10
+print(myInt) #10
+
+#Float : Decimal Values
+myFloat = 1.1
+print(myFloat) #1.1
+
+#Boolean : True or False
+myBool = 1>2
+print(myBool) #False
+myBool1 = 2<3
+print(myBool1) #True
+
+#String : English Sentence compromising of letters, words and phrases
+mySent = "Hello World"
+print(mySent) #Hello World
+
+#To check the data type : type()
+print(type(myInt)) #<class 'int'> 
+print(type(myFloat)) #<class 'float'> 
+print(type(myBool)) #<class 'bool'> 
+print(type(mySent)) #<class 'str'> 
